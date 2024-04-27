c7aed36c32d68b3c3422a1350ccbd58d582f2620
Lucas Panadero <lucasn.panadero@gmail.com>
2024-04-27 20:35:38 -0300

    27/04

--- views/categoria.php
+++ views/categoria.php
@@ -18,7 +18,6 @@ $comics = (new Comic())->catalogo_x_personaje($serieSeleccionada);
         <h3><?= $comic->getTitulo() ?></h3>
         <p><?= $comic->getSubtitulo() ?></p>
         <p class="price">$<?= $comic->getPrecio() ?></p>
-        <button class="add boton" data-id="1" data-val="120" data-cat="Buzos">Agregar al carrito</button>
       </a>
     </article>
 
